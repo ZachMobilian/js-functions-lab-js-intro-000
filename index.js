e03e85a2e649ae16db396bde8ff91b9fd6565b51
Zach Mobilian <zach.mobilian@blackrock.com>
2019-03-15 19:45:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy holidays!";
+  return 'Happy holidays!';
 }
 
 function happyHolidaysTo(name){
-  return "Happy holidays, you!";
+  return "Happy holidays, ${name}!";
 }
 
 function happyHolidayto(holiday, name){
-  return "Happy holiday, you!";
+  return "Happy ${holiday}, ${name}!";
 }
 
 function holidayCountdown(holiday, days){
